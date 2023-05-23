04577f27dbd451c160c92e7dba183064a6aee6e7
iamlateforbreakfast <iamlateforbreakast@users.noreply.github.com>
2023-05-23 09:39:16 +0100

    Create Object.h

---
+++ src/Application/Object.h
@@ -0,0 +1,40 @@
+//
+// Object.h
+// 
+//
+//
+
+class Object;
+
+#ifndef _OBJECT_H_
+#define _OBJECT_H_
+
+#include <Logger.h>
+#include <string>
+
+class Application;
+
+class Object
+{
+   public:
+      Object(Object *parent=0, string name="");
+      ~Object();
+      void         info(const char* format ...) const;
+      void         error(const char* format ...) const;
+      void         debug(const char* format ...) const;
+      void         fatal(const char* format ...) const;
+      virtual void save(const string fileName) const = 0;
+      virtual void load(const string fileName) = 0;
+      virtual void print() const = 0;
+      unsigned int size() const;
+   protected:
+      static Application *appl;
+      static Logger *logger;
+      Object      *parent;
+      string       object_name;
+   private:
+      Object(const Object &);
+      Object & operator = (const Object &);
+};
+
+#endif

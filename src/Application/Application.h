c25d2f735907ea3d71ca66d7d11a31122d4932b2
iamlateforbreakfast <iamlateforbreakast@users.noreply.github.com>
2023-05-23 09:42:15 +0100

    Create Application.h

---
+++ src/Application/Application.h
@@ -0,0 +1,60 @@
+//
+// Application.h:
+// Header file for Application.cpp
+//
+//
+
+class Application;
+
+#ifndef  _APPLICATION_H_
+#define  _APPLICATION_H_
+
+#include <windows.h>
+#include <string>
+
+#include <Object.h>
+#include <CommandLine.h>
+#include <Options.h>
+#include <Struct.h>
+#include <MMI.h>
+#include <AudioPlayer.h>
+#include <GameWorld.h>
+#include <Renderer.h>
+
+class Application : public Object
+{
+   public:
+      Application(int argc, char**argv);     
+      ~Application();
+      void                start();
+      void                terminate();
+      void                help() const;
+      string              getVersion() const;
+      GameRenderer*       getRenderer() const;
+      string              getStringOption(string optionname);
+      virtual void        save(const string fileName) const;
+      virtual void        load(const string fileName);
+      void                print() const;
+      unsigned int        size() const;
+      
+   private:
+      string              name;
+      string              version;
+      CommandLine        *commandLine;
+      Options            *options;
+      MMI                *mmi;
+      GameRenderer       *renderer;
+      GameWorld          *world;
+      AudioPlayer        *audio;
+
+      void handle_main_menu_events(MMIEvent MMI_event);
+      void handle_options_menu_events(MMIEvent MMI_event);
+      void handle_mission_selection_events(MMIEvent MMI_events);
+      void handle_agents_selection_events(MMIEvent MMI_events);
+      void handle_corporation_creation_events(MMIEvent MMI_events);
+      void handle_mission_events(MMIEvent MMI_events);
+      
+      Application(const Application &);
+      Application & operator = (const Application &);
+};
+#endif

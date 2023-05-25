c0215f8094cb06eea6ccfbba26ab066323d01774
iamlateforbreakfast <iamlateforbreakast@users.noreply.github.com>
2023-05-25 08:54:21 +0100

    Update Pixmap.h

--- src/Renderer/Pixmap.h
+++ src/Renderer/Pixmap.h
@@ -1,1 +1,34 @@
+//
+//
+//
+//
+//
+
+#ifndef _PIXMAP_H_
+#define _PIXMAP_H_
+
+#include <string>
+#include <SDL/SDL_image.h>
+#include <Object.h>
+
+class Pixmap : public Object
+{
+   public:
+      Pixmap();
+      Pixmap(string filename);
+      ~Pixmap();
+      unsigned int getWidth() const;
+      unsigned int getHeight() const;
+      void setPixmap(SDL_Surface* image);
+      SDL_Surface* getPixmap() const;
+      void save(const string fileName) const;
+      void load(const string fileName);
+      void print() const;
+   private:
+      unsigned int width;
+      unsigned int height;
+      SDL_Surface* data;
+};
+
+#endif
 
